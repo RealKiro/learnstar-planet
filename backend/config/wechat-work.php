5b2e2eb8a68f2f61707ccc7b3dafccfbea373a55
RealKiro <zuoqihe@gmail.com>
2026-07-13 00:44:50 +0800

    chore: add missing config and migrations for wechat work integration

---
+++ backend/config/wechat-work.php
@@ -0,0 +1,2 @@
+<?php
+return ['corp_id'=>env('WECOM_CORP_ID',''),'agent_id'=>(int)env('WECOM_AGENT_ID',0),'secret'=>env('WECOM_SECRET',''),'token'=>env('WECOM_TOKEN',''),'encoding_aes_key'=>env('WECOM_ENCODING_AES_KEY',''),'leave_template_id'=>env('WECOM_LEAVE_TEMPLATE_ID',''),];
